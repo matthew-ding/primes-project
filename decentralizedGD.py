d0fac2194c6333d66b8e84ccfb223bca1aff5217
Matthew Ding <matthew.c.ding2012@gmail.com>
2020-05-22 22:50:27 -0400

    added byzantine nodes and geometric median

--- decentralizedGD.py
+++ decentralizedGD.py
@@ -4,33 +4,40 @@ from scipy.spatial.distance import cdist, euclidean
 
 ### HYPERPARAMETERS
 n = 30  # number of dimensions
-m = 9  # number of nodes
-byzantine_set = [4, 5]  # set of byzantine nodes
-adjList = {0: [3], 1: [3], 2: [3], 3: [0, 1, 2, 4, 5],
-           4: [3, 6], 5: [3, 6], 6: [4, 5, 7, 8], 7: [6], 8: [6]}
+m = 8  # number of nodes
+byzantine_set = [0, 1, 7]  # set of byzantine nodes
+# adjList = {0: [3], 1: [3], 2: [3], 3: [0, 1, 2, 4, 5],
+#           4: [3, 6], 5: [3, 6], 6: [4, 5, 7, 8, 9], 7: [6], 8: [6], 9: [6]}
 
-dataset = {}
+adjList = {0: [4, 5], 1: [4, 5], 2: [4, 5], 3: [4, 5],
+           4: [0, 1, 2, 3, 6], 5: [0, 1, 2, 3, 6], 6: [4, 5, 7], 7: [6]}
+
+dataset = []
 for i in range(m):
-    if i not in byzantine_set:
-        list = []
-        for j in range(n):
-            list.append(np.random.normal(i, 10))
-        dataset[i] = list
+    list = []
+    for j in range(n):
+        list.append(abs(float(np.random.normal(j, 0))))
+    dataset.append(list)
 
 # dictionary of variables
 symbolDict = {}
 for i in range(n):
     symbolDict["x" + str(i)] = Symbol("x" + str(i))
 
-f = symbolDict["x0"] ** 2
-for i in range(1, n):
-    f = f + i * (symbolDict["x" + str(i)] - i) ** 2
+functionDict = []
+for i in range(m):
+    f = 0
+    for j in range(n):
+        expression = dataset[i][j] * (symbolDict["x" + str(j)] - dataset[i][j]) ** 2.0
+        f += expression
+    functionDict.append(f)
 
 
-def function():
-    output = f
+# output cost function for machine j
+def function(j):
+    output = functionDict[j]
     for i in range(n):
-        output = output.subs(symbolDict["x" + str(i)], thetaDict["x" + str(i)])
+        output = output.subs(symbolDict["x" + str(i)], thetaDict[j][i])
 
     return output
 
@@ -64,64 +71,73 @@ def geometric_median(X, eps=1e-5):
         y = y1
 
 
-X = np.array([[1,1], [5,5]])
-print(geometric_median(X))
-
-# dictionary of partial derivatives
-gradientDict = {}
-for i in range(n):
-    gradientDict["x" + str(i)] = f.diff(symbolDict["x" + str(i)])
+# dictionary of partial derivatives (n x m dimensions)
+gradientDict = []
+for j in range(m):
+    row = []
+    for i in range(n):
+        row.append(functionDict[j].diff(symbolDict["x" + str(i)]))
+    gradientDict.append(row)
 
 # Data
 # dictionary of parameters
-thetaDict = {}
-for i in range(n):
-    thetaDict["x" + str(i)] = i * 100
-alpha = .01
+thetaDict = []
+
+for j in range(m):
+    row = []
+    for i in range(n):
+        row.append(i * 100)
+    thetaDict.append(row)
+
+thetaDict = np.array(thetaDict, dtype=float)
+
+alpha = .005
 iterations = 0
-check = 0
-precision = 1 / 1000000
 printData = True
-maxIterations = 5000
 
-tempThetaDict = {}
-while True:
-    for i in range(n):
-        gradient = gradientDict["x" + str(i)]
+while iterations < 150:
+    calculatedGradients = {}
 
-        for j in range(n):
-            gradient = gradient.subs(symbolDict["x" + str(j)], thetaDict["x" + str(j)])
+    # iterating through machines
+    for z in range(m):
+        calculatedGradients[z] = []
+        # iterating through dimensions
+        for i in range(n):
+            gradient = gradientDict[z][i]
 
-        tempThetaDict["x" + str(i)] = thetaDict["x" + str(i)] - alpha * N(gradient).evalf()
+            for j in range(n):
+                gradient = gradient.subs(symbolDict["x" + str(j)], thetaDict[z][j])
 
-    # If the number of iterations goes up too much, maybe the parameters
-    # are diverging! Let's stop the loop and try to understand.
+            gradient = float(gradient)
+
+            if z in byzantine_set:
+                gradient *= -1.0
+
+            calculatedGradients[z].append(gradient)
+
+    # Simultaneous update of gradients, based on neighbors
+    for i in range(m):
+        currentNeighborGrad = []
+
+        for j in range(m):
+            if j in adjList[i] or j == i:
+                currentNeighborGrad.append(calculatedGradients[j])
+
+        currentNeighborGrad = np.array(currentNeighborGrad, dtype=float)
+
+        ### TODO: Fix geometric median update
+        thetaDict[i] = thetaDict[i] - alpha * geometric_median(
+            currentNeighborGrad)  # np.array(calculatedGradients[i], dtype=float)
+
+    # if (i==5):
+    #     print(geometric_median(currentNeighborGrad))
+    #     print(np.array(calculatedGradients[i], dtype=float))
+
     iterations += 1
-    if iterations > maxIterations:
-        print("Too many iterations. Adjust alpha and make sure that the function is convex!")
-        printData = False
-        break
-
-    # If the value of theta changes less of a certain amount, our goal is met.
-    nextIteration = false
-    for i in range(n):
-        if abs(tempThetaDict["x" + str(i)] - thetaDict["x" + str(i)]) > precision:
-            nextIteration = True
-            continue
-
-    if not nextIteration:
-        break
-
-    # Simultaneous update
-    for i in range(n):
-        thetaDict["x" + str(i)] = tempThetaDict["x" + str(i)]
-
     # Cost function output
-    if iterations % 50 == 0:
-        print("Iteration " + str(iterations) + ", Cost: " + str(function()))
+    if iterations % 10 == 0:
+        print("Iteration " + str(iterations) + ", Cost: " + str(function(4)))
 
 if printData:
-    print("The function " + str(f) + " converges to a minimum")
-    print("Number of iterations:", iterations, sep=" ")
     for i in range(n):
-        print("theta (x" + str(i) + ") =", thetaDict["x" + str(i)], sep=" ")
+        print("theta (x" + str(i) + ") =", thetaDict[0][i], sep=" ")
