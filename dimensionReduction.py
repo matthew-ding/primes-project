6264464caea79bb16496afefa530c4426ad1fbdd
Matthew Ding <matthew.c.ding2012@gmail.com>
2020-05-24 14:33:45 -0400

    added random projection matrix

--- dimensionReduction.py
+++ dimensionReduction.py
@@ -1,27 +1,41 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+from sklearn import random_projection
 
-d = 30  # number of dimensions
+d = 1000  # number of dimensions
 
 # my_data = pd.read_csv('home.txt', names=["size", "bedroom", "price"])
 my_data = pd.read_csv('dataset.txt')  # , names=nm)
 
-print(my_data)
-
-# we need to normalize the features using mean normalization
-# my_data = (my_data - my_data.mean()) / my_data.std()
-
 # setting the matrixes
+
+# Setting up costs (y values)
+y = my_data.iloc[:, d:d + 1].values  # .values converts it from pandas.core.frame.DataFrame to numpy.ndarray
+
+# projection of input vectors (x values)
+transformer = random_projection.GaussianRandomProjection(eps=0.5)
+
+# generating random gaussian matrix
+transformer.fit(np.array(my_data))
+projection_matrix = transformer.components_
+
+my_data = transformer.transform(my_data)
+
+# new number of dimension
+d = len(my_data[0])
+
+# parameter array
+theta = np.zeros([1, d + 1])
+
+my_data = pd.DataFrame(my_data)
+
+# Setting up X-Values
 X = my_data.iloc[:, 0:d]
 ones = np.ones([X.shape[0], 1])
 X = np.concatenate((ones, X), axis=1)
 
-y = my_data.iloc[:, d:d + 1].values  # .values converts it from pandas.core.frame.DataFrame to numpy.ndarray
-theta = np.zeros([1, d + 1])
 
-
-# computecost
+# compute cost
 def computeCost(X, y, theta):
     tobesummed = np.power(((X @ theta.T) - y), 2)
     return np.sum(tobesummed) / (2 * len(X))
@@ -35,7 +49,7 @@ def gradientDescent(X, y, theta, iters, alpha):
         theta = theta - (alpha / len(X)) * np.sum(X * (X @ theta.T - y), axis=0)
         cost.append(computeCost(X, y, theta))
 
-        if iters % 50 == 0:
+        if iters % 1000 == 0:
             print("Cost at iteration " + str(iters) + ": " + str(cost[iters]))
 
         if iters != 1:
@@ -46,9 +60,9 @@ def gradientDescent(X, y, theta, iters, alpha):
 
 
 # set hyper parameters
-alpha = 0.01
+alpha = 0.0000015
 iters = 0
-precision = 0.0000001
+precision = 0.00001
 
 g, cost = gradientDescent(X, y, theta, iters, alpha)
 
@@ -58,12 +72,6 @@ print("Converges, Final Cost: " + str(finalCost) + "\n")
 g = g.tolist()[0]
 
 for i in range(1, len(g)):
-    print("Variable: x" + str(i+1) + ", Coefficient: " + str(g[i]))
+    print("Variable: x" + str(i) + ", Coefficient: " + str(g[i]))
 
 print("Constant: " + str(g[0]))
-
-# fig, ax = plt.subplots()
-# ax.plot(np.arange(iters), cost, 'r')
-# ax.set_xlabel('Iterations')
-# ax.set_ylabel('Cost')
-# ax.set_title('Error vs. Training Epoch')
