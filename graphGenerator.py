af158ca15ba0c1999fcd88baf645f091866f55f8
Matthew Ding <matthew.c.ding2012@gmail.com>
2020-05-30 15:51:16 -0400

    completed generator with byzantine neighbors

--- graphGenerator.py
+++ graphGenerator.py
@@ -1,25 +1,51 @@
+import random
 from networkx import *
+import matplotlib.pyplot as plt
 
-byzantine_size = 4
-honest_size = 6
-p = 0.5
-honest_subgraph = erdos_renyi_graph(honest_size, p)
-byzantine_subgraph = erdos_renyi_graph(byzantine_size, p)
+byzantine_size = 10  # number of byzantine nodes
+honest_size = 10  # number of honest nodes
 
+# graph generation
+# honest graph is connected
+while True:
+    honest_subgraph = erdos_renyi_graph(honest_size, 0.8)
+    if is_connected(honest_subgraph):
+        break
+# byzantine graph is disconnected
+while True:
+    byzantine_subgraph = erdos_renyi_graph(byzantine_size, 0.2)
+    if not is_connected(byzantine_subgraph):
+        break
+
+# labeling nodes
 for i in range(byzantine_size):
-    byzantine_subgraph.nodes
-    {"byzantine": True}
+    byzantine_subgraph.nodes[i]['byzantine'] = True
 
 for i in range(honest_size):
-    honest_subgraph.nodes
-    {"byzantine": False}
+    honest_subgraph.nodes[i]['byzantine'] = False
 
-convert_node_labels_to_integers(byzantine_subgraph, honest_size+1)
+byzantine_subgraph = convert_node_labels_to_integers(byzantine_subgraph, first_label=honest_size
+                                , ordering='default', label_attribute=None)
 
-print(byzantine_subgraph.nodes)
-print(honest_subgraph.nodes)
+# printing graphs
+draw_networkx(honest_subgraph)
+plt.savefig("honest.png")
+plt.clf()
+draw_networkx(byzantine_subgraph, node_color="r")
+plt.savefig("byzantine.png")
+plt.clf()
 
-print(byzantine_subgraph.nodes.data())
-print(honest_subgraph.nodes.data())
+final_graph = compose(honest_subgraph, byzantine_subgraph)
 
+for i in range(honest_size):
+    max_byzantine = len(list(honest_subgraph.neighbors(i)))//2
+    byzantine_num = random.randrange(int(0.75*max_byzantine), max_byzantine+1)
 
+    entire_byzantine_set = list(byzantine_subgraph.nodes)
+    byzantine_set = random.sample(entire_byzantine_set, byzantine_num)
+
+    for j in byzantine_set:
+        final_graph.add_edge(i,j)
+
+draw_networkx(final_graph)
+plt.savefig("final_graph.png")
